0844d731daa0c936b721bf94b6ce27a2f14777a9
Gaston Boulanger <gaston.fb87@gmail.com>
2022-03-18 20:14:59 -0300

    prevent null item.title

--- pages/buffet/itemOrder.js
+++ pages/buffet/itemOrder.js
@@ -5,7 +5,7 @@ export default function ItemOrder ({item, click}) {
     return (
         <div className="grid grid-cols-5 py-2">
             <div className="col-span-2">
-                <p>{item.title}</p>
+                <p>{item?.title}</p>
             </div>
             <div className="flex items-center justify-center">
                 <button onClick={() => click('remove', item)} className="h-8 w-8 py-2 bg-black text-white rounded-full flex items-center justify-center">
@@ -13,7 +13,7 @@ export default function ItemOrder ({item, click}) {
                 </button>
             </div>
             <div className="flex items-center justify-center">
-                <p>{item.count ? item.count : 0}</p>
+                <p>{item?.count ? item.count : 0}</p>
             </div>
             <div className="flex items-center justify-center">
                 <button onClick={() => click('add', item)} className="h-8 w-8 py-2 bg-black text-white rounded-full flex items-center justify-center">
